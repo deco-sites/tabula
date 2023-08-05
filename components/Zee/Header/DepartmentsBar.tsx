aab4cf973163d9797e849ee3eefacf5402a276d3
Eduardo Brito <eduardo.guardian@gmail.com>
2023-08-04 23:37:16 -0300

    fix header margins

--- components/Zee/Header/DepartmentsBar.tsx
+++ components/Zee/Header/DepartmentsBar.tsx
@@ -1,15 +1,15 @@
 export default function DepartmentsBar() {
   return (
-    <div class="flex justify-between w-full border-b bg-gray-10">
+    <div class="flex justify-between w-full border-b border-gray-20 bg-gray-10 h-[40px]">
       <nav class="flex xs:max-lg:w-full">
-        <ol class="flex row nowrap w-full h-[40px] divide-x divide-solid">
+        <ol class="flex row nowrap w-full divide-x divide-gray-20">
           <li class="grow xs:max-lg:w-1/3 lg:w-[140px]">ZeeDog</li>
           <li class="grow xs:max-lg:w-1/3 lg:w-[140px]">Kitchen</li>
           <li class="grow xs:max-lg:w-1/3 lg:w-[140px]">Human</li>
         </ol>
       </nav>
       <nav class="hidden lg:flex">
-        <ol class="flex row nowrap w-full h-[40px] divide-x divide-solid">
+        <ol class="flex row nowrap w-full divide-x divide-gray-20">
           <li class="">Assinatura</li>
           <li class="">10% OFF na primeira compra</li>
           <li class="">Ganhe R$ 25</li>
--- components/Zee/Header/HeaderBar.tsx
+++ components/Zee/Header/HeaderBar.tsx
@@ -1,6 +1,6 @@
 export default function HeaderBar() {
   return (
-    <div class="h-[70px] border-b">
+    <div class="h-[70px] bg-white lg:border-b border-gray-20">
       Header Bar
     </div>
   );
